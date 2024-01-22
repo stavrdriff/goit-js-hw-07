61fd4a40302e186806a061b018a7df1664b4e66b
Tymofii <tymofii.wga@gmail.com>
2024-01-22 21:29:19 +0200

    ended task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+const buttonHandler = () => {
+  const widget = document.querySelector('.widget');
+  const button = widget.querySelector('.change-color');
+  const text = widget.querySelector('.color');
+
+  button.addEventListener('click', () => {
+    text.textContent = getRandomHexColor();
+  })
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonHandler();
