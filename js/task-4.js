1cc46f71722d27a3fe8688739c59d8c8c7cdf8fb
Tymofii <tymofii.wga@gmail.com>
2024-01-22 21:03:52 +0200

    ended task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+'use strict'
 
+const submitForm = () => {
+  const form = document.querySelector('form.login-form');
+  const emailInput = form.querySelector('[type="email"]');
+  const passInput = form.querySelector('[type="password"]');
+  const elements = {};
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!emailInput.value.length || !passInput.value.length) {
+      alert('All form fields must be filled in');
+    }
+    else {
+      elements.email = emailInput.value.trim();
+      elements.password = passInput.value.trim();
+
+      console.log(elements);
+      form.reset();
+    }
+  })
+}
+
+submitForm();
